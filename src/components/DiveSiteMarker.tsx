485b1aaa2b8fd40eabf141c779a563e9a7e3c1f8
Francesc Montserrat <fmontserrat.carvajal@gmail.com>
2021-12-15 15:28:40 +0100

    site icon

--- src/components/DiveSiteMarker.tsx
+++ src/components/DiveSiteMarker.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Marker as LeafletMarker } from 'leaflet'
 import { SubsurfaceDive } from '../model/model'
 import DiveSitePopup from './DiveSitePopup'
+import { siteMarkerIcon } from './icons'
 
 interface DiveSiteMarkerProps {
   marker: LeafletMarker
@@ -11,7 +12,7 @@ interface DiveSiteMarkerProps {
 
 const DiveSiteMarker = ({ marker, dives }: DiveSiteMarkerProps): React.ReactElement => {
   return (
-    <Marker position={marker.getLatLng()} title={marker.options.title}>
+    <Marker position={marker.getLatLng()} title={marker.options.title} icon={siteMarkerIcon}>
       <DiveSitePopup dives={dives} marker={marker} />
     </Marker>
   )
---
+++ src/components/icons.tsx
@@ -0,0 +1,8 @@
+import { icon } from 'leaflet'
+
+export const siteMarkerIcon = icon({
+  iconUrl: 'img/site.png',
+  iconSize: [32, 32],
+  iconAnchor: [0, 0],
+  popupAnchor: [16, 0],
+})
